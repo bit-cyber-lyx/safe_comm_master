054b938728d73511ee6580839173d16e09c6dd7c
lyx7627 <52841986+lyx7627@users.noreply.github.com>
2023-10-12 00:02:45 +0800

    Add files via upload

---
+++ decode24.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+//  _____                                            
+// /__   \  ___  ___  _ __    ___   __ _  _ __   ___ 
+//   / /\/ / _ \/ __|| '_ \  / _ \ / _` || '__| / _ \
+//  / /   |  __/\__ \| |_) ||  __/| (_| || |   |  __/
+//  \/     \___||___/| .__/  \___| \__,_||_|    \___|
+//                   |_|                              
+// Create Date: 12/10/2022 08:51:30 PM
+// Design Name: 
+// Module Name: decode24
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module decode24(
+    input       [1:0] a     ,       
+    output reg  [3:0] led    	
+);
+
+
+always@(a)                    
+	begin
+		case(a)
+			2'b00: led = 4'b0001; 
+			2'b01: led = 4'b0010;
+			2'b10: led = 4'b0100;
+			2'b11: led = 4'b1000;
+		endcase
+	end
+
+endmodule
---
+++ decode24_tb.v
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+//  _____                                            
+// /__   \  ___  ___  _ __    ___   __ _  _ __   ___ 
+//   / /\/ / _ \/ __|| '_ \  / _ \ / _` || '__| / _ \
+//  / /   |  __/\__ \| |_) ||  __/| (_| || |   |  __/
+//  \/     \___||___/| .__/  \___| \__,_||_|    \___|
+//                   |_|                              
+// Create Date: 12/10/2022 08:51:30 PM
+// Design Name: 
+// Module Name: decode24_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+ 
+module decode24_tb();       
+ 
+reg    [1:0] a;        
+wire   [3:0] led;      	
+ 
+//初始化过程块
+initial
+begin
+	a = 2'b00;
+	#20
+	a = 2'b01;
+	#20
+	a = 2'b10;
+	#20
+	a = 2'b11;
+	#20
+	a = 2'b00;
+end
+
+decode24  decode24_inst (     
+	.a(a),    
+	.led(led)  
+);
+endmodule
